8c0aa8e75fc45a219090f8e610cdb16ae0193ba3
AchrafGarai <32675431+AchrafGarai@users.noreply.github.com>
2025-02-05 15:51:30 +0100

    dark mode assets

--- src/components/projects/artboard.tsx
+++ src/components/projects/artboard.tsx
@@ -1,17 +1,36 @@
+import Image from "next/image";
 interface ArtboardProps extends React.HTMLAttributes<HTMLDivElement> {
-	title: string;
-	href?: string;
+  title: string;
+  href?: string;
 }
 
-function Artbaord({ title }: ArtboardProps) {
-	return (
-		<>
-			<div className=" flex flex-col gap-2 items-start">
-				<span className=" text-xs text-muted-foreground">{title}</span>
-				<div className="border bg-accent w-[960px] h-[540px] rounded-lg hover:border-sky-500 " />
-			</div>
-		</>
-	);
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  title: string;
+  href?: string;
+}
+
+function Artbaord({ title, className, children, href }: Props) {
+  return (
+    <>
+      <div className=" flex flex-col gap-2 items-start">
+        <span className=" text-xs text-muted-foreground">{title}</span>
+        <div
+          className={` flex items-center justify-center border bg-accent w-[960px] h-[540px] rounded-lg hover:border-sky-500 ${className}`}
+        >
+          {children}
+          {href && (
+            <Image
+              src={href}
+              width={700}
+              height={700}
+              alt=""
+              className=" shadow-2xl"
+            />
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Artbaord;
