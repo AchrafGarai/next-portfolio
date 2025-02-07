466acc22631868aff547204981866bd2f77de0a7
AchrafGarai <32675431+AchrafGarai@users.noreply.github.com>
2025-02-07 21:49:51 +0100

    update header

--- src/app/work/ui/aichor/page.tsx
+++ src/app/work/ui/aichor/page.tsx
@@ -2,44 +2,44 @@ import Artbaord from "@/components/projects/artboard";
 import ProjectHeader from "@/components/projects/project-header";
 import BrandingGalley from "@/components/sections/branding-callery";
 const ImageSouurce = {
-  lightSrc: "/aichor/aichor-experiments.png",
-  darkSrc: "/aichor/aichor-experiments-dark.png",
+	lightSrc: "/aichor/aichor-experiments.png",
+	darkSrc: "/aichor/aichor-experiments-dark.png",
 };
 
 function Page() {
-  return (
-    <>
-      <ProjectHeader
-        title={"AIchor"}
-        description={
-          "Lorem ipsum dolor sit amet consectetur. Sodales ac tellus eu velit. At vel"
-        }
-        imageSrc={ImageSouurce}
-      />
-      <div className="flex flex-grow  h-screen justify-center items-center ">
-        <div className="max-w-2xl ">
-          <div className=" flex flex-col gap-6 justify-center items-center">
-            <p>
-              Lorem ipsum dolor sit amet consectetur. Sodales ac tellus eu
-              velit. At vel
-            </p>
-            <h3 className="text-6xl font-tanker text-center">
-              Discover the world's largest shared adventure, crafted by everyone
-            </h3>
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-col flex-grow min-h-screen justify-center items-center gap-40">
-        <Artbaord
-          title="Artboard 1"
-          className=" bg-gradient-to-b from-sky-400 to-indigo-600 "
-          href="/aichor/aichor-experiments.png"
-        />
-        <Artbaord title="Artboard 1" />
-      </div>
-      <BrandingGalley />
-    </>
-  );
+	return (
+		<>
+			<ProjectHeader
+				title={"Aichor-Building a next Gen AI Platform"}
+				description={
+					"Lorem ipsum dolor sit amet consectetur. Sodales ac tellus eu velit. At vel"
+				}
+				imageSrc={ImageSouurce}
+			/>
+			<div className="flex flex-grow  h-screen justify-center items-center ">
+				<div className="max-w-2xl ">
+					<div className=" flex flex-col gap-6 justify-center items-center">
+						<p>
+							Lorem ipsum dolor sit amet consectetur. Sodales ac tellus eu
+							velit. At vel
+						</p>
+						<h3 className="text-6xl font-tanker text-center">
+							Discover the world's largest shared adventure, crafted by everyone
+						</h3>
+					</div>
+				</div>
+			</div>
+			<div className="flex flex-col flex-grow min-h-screen justify-center items-center gap-40">
+				<Artbaord
+					title="Artboard 1"
+					className=" bg-gradient-to-b from-sky-400 to-indigo-600 "
+					href="/aichor/aichor-experiments.png"
+				/>
+				<Artbaord title="Artboard 1" />
+			</div>
+			<BrandingGalley />
+		</>
+	);
 }
 
 export default Page;
--- src/components/projects/project-header.tsx
+++ src/components/projects/project-header.tsx
@@ -4,40 +4,41 @@ import Image from "next/image";
 import ThemedImage from "./themed-image";
 
 function ProjectHeader({
-  title,
-  description,
-  imageSrc,
+	title,
+	description,
+	imageSrc,
 }: {
-  title: string;
-  description: string;
-  imageSrc?: { lightSrc: string; darkSrc: string };
+	title: string;
+	description: string;
+	imageSrc?: { lightSrc: string; darkSrc: string };
 }) {
-  return (
-    <div className="flex flex-col flex-grow justify-center items-center py-12 ">
-      <div className="relative mb-12">
-        {/* <Artbaord title="Artboard 1" /> */}
-        {/* <div className="h-96 bg-gradient-to-t from-background via-50% w-full absolute bottom-0" /> */}
-        {imageSrc && (
-          <div className="p-2 bg-gradient-to-b from-accent  rounded-3xl backdrop-blur border ">
-            <ThemedImage
-              srcLight={imageSrc.lightSrc}
-              srcDark={imageSrc.darkSrc}
-              alt={title}
-              width={960}
-              height={540}
-              className="border-4 rounded-2xl shadow-2xl "
-            />
-          </div>
-        )}
-      </div>
-      <div className="max-w-2xl mb-9  ">
-        <div className=" flex flex-col gap-6 justify-center items-center">
-          <h3 className="text-8xl font-tanker text-center">{title}</h3>
-          <p className="text-muted-foreground">{description}</p>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex flex-col flex-grow justify-center items-center py-12 min-h-screen ">
+			<div className="relative mb-12">
+				{/* <Artbaord title="Artboard 1" /> */}
+				{/* <div className="h-96 bg-gradient-to-t from-background via-50% w-full absolute bottom-0" /> */}
+
+				{imageSrc && (
+					<div className="p-2 bg-gradient-to-b from-accent  rounded-3xl backdrop-blur border absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[960px] z-0 opacity-20 ">
+						<ThemedImage
+							srcLight={imageSrc.lightSrc}
+							srcDark={imageSrc.darkSrc}
+							alt={title}
+							width={960}
+							height={540}
+							className="border-4 rounded-2xl shadow-2xl "
+						/>
+					</div>
+				)}
+			</div>
+			<div className="max-w-3xl mb-9 absolute z-30 ">
+				<div className=" flex flex-col gap-6 justify-center items-center">
+					<p className="text-muted-foreground">Case Study</p>
+					<h3 className="text-8xl font-tanker text-center">{title}</h3>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ProjectHeader;
